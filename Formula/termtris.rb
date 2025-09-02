abd68df1edc6969c89dace76230c761a12419adf
mohammad ali <prof.mohdali@outlook.com>
2025-09-02 21:39:39 +0530

    updated

--- Formula/termtris.rb
+++ Formula/termtris.rb
@@ -1,7 +1,7 @@
 class Termtris < Formula
     desc "A terminal Tetris game with persistent highscores (curses + SQLite)"
     homepage "https://github.com/shay-ff/termtris"
-    url "https://github.com/shay-ff/termtris/archive/refs/tags/v0.1.0.tar.gz"
+    url "https://github.com/shay-ff/termtris/archive/refs/tags/v0.2.0.tar.gz"
     sha256 "f40e407c1dc70267b788a9b6b2b12b9bb5f191a2a8c6ec10ed1ca98c8d70e6fd"
     license "MIT"
   
